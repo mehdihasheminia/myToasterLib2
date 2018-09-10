407317699bb72171fcdbfb6559e54e51442bf347
Hashemi <mehdihasheminia@gmail.com>
2018-09-10 12:59:09 +0430

    second func added

--- toasterlibrary/src/main/java/com/bornaapp/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/bornaapp/toasterlibrary/ToasterMessage.java
@@ -8,4 +8,8 @@ public class ToasterMessage {
     public static void s(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
     }
+
+    public static void s2(Context c, String message) {
+        Toast.makeText(c, message, Toast.LENGTH_LONG).show();
+    }
 }
